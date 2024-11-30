5ae501ad66171b6fc4d722299accea2fb8c26bb5
summerr0-0 <summerr060@gmail.com>
2024-11-30 22:16:59 +0900

    add nextConfig

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Next.js 정적 페이지 빌드를 위한 설정
+    output: 'export',
+    images: {
+        // 정적 이미지 최적화를 위한 기본 설정
+        formats: ['image/avif', 'image/webp'],
+        dangerouslyAllowSVG: true, // SVG 이미지를 허용
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    },
+    reactStrictMode: true, // React의 엄격한 모드 활성화
+    swcMinify: true, // SWC 기반 코드 최소화 활성화
+};
+
+module.exports = nextConfig;
